4aec061eff3123edd62c4e29c6f33c3d10697f98
bengillies <bengillies@gmail.com>
2010-05-04 16:09:22 +0100

    updated the release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ AUTHOR = 'Ben Gillies'
 AUTHOR_EMAIL = 'bengillies@gmail.com'
 NAME = 'tiddlywebplugins.form'
 DESCRIPTION = 'Adds POST and binary file upload (via HTML) support to TiddlyWeb'
-VERSION = '0.1'
+VERSION = '0.2'
 
 
 import os
